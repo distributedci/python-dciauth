62371407f90416b4cd41e0c7a2b601920e4d813c
Guillaume Vincent <gvincent@redhat.com>
2017-11-06 17:09:47 +0100

    First commit
    Change-Id: I2e93d70bb6be7bf44486d1b7bb9dbf1eb76d5da6

---
+++ example/server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License'); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from flask import Flask
+from flask import request
+from flask import jsonify
+from dciauth import signature
+
+app = Flask(__name__)
+
+
+@app.route("/api/v1/jobs", methods=['GET', 'POST'])
+def get_jobs():
+    secret = "Y4efRHLzw2bC2deAZNZvxeeVvI46Cx8XaLYm47Dc019S6bHKejSBVJiGAfHbZLIN"
+    headers = request.headers
+    expected_signature = signature.calculate_signature(
+        secret,
+        method=request.method,
+        headers=headers,
+        url=request.path,
+        query_string=request.query_string.decode('utf-8'),
+        payload=request.get_json(silent=True)
+    )
+    dci_signature = signature.get_signature_from_headers(headers)
+
+    if expected_signature != dci_signature:
+        raise Exception("Authentication failed: signature invalid")
+
+    if signature.is_expired(headers):
+        raise Exception("Authentication failed: signature expired")
+    return jsonify({'jobs': []})
---
+++ tests/test_signature.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License'); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from freezegun import freeze_time
+from dciauth import signature
+
+
+def test_hash_payload():
+    payload = {"foo": "bar"}
+    expected_hash = "426fc04f04bf8fdb5831dc37bbb6dcf70f63a37e05a68c6ea5f63e85ae579376"
+    assert signature._hash_payload(payload) == expected_hash
+
+
+def test_hash_payload_unicode():
+    payload = {"foo": "I ❤ bar"}
+    expected_hash = "74816a5237d72280aa327ed345267b4c31ee9f7dad0686e14940918c34a5533a"
+    assert signature._hash_payload(payload) == expected_hash
+
+
+def test_hash_payload_for_get_request_is_empty_string():
+    payload = None
+    expected_hash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    assert signature._hash_payload(payload) == expected_hash
+
+
+@freeze_time("20171103T162727Z")
+def test_not_a_replay_request():
+    one_minute_ago = '20171103T162627Z'
+    headers = {
+        'DCI-Datetime': one_minute_ago
+    }
+    assert signature.is_expired(headers) is False
+
+
+@freeze_time("20171103T162727Z")
+def test_five_minutes_after_is_a_replay_request():
+    more_thant_five_minute_ago = '20171103T162226Z'
+    headers = {
+        'DCI-Datetime': more_thant_five_minute_ago
+    }
+    assert signature.is_expired(headers)
+
+
+def test_create_string_to_sign():
+    method = "GET"
+    content_type = "application/json"
+    timestamp = "20171103T162727Z"
+    url = "/api/v1/jobs"
+    query_string = "limit=100&offset=1"
+    hashed_payload = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+    string_to_sign = signature._create_string_to_sign(
+        method,
+        content_type,
+        timestamp,
+        url,
+        query_string,
+        hashed_payload,
+    )
+
+    assert string_to_sign == '''GET
+application/json
+20171103T162727Z
+/api/v1/jobs
+limit=100&offset=1
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'''
+
+
+def test_calculate_signature():
+    secret = "Y4efRHLzw2bC2deAZNZvxeeVvI46Cx8XaLYm47Dc019S6bHKejSBVJiGAfHbZLIN"
+    method = "GET"
+    headers = {
+        'DCI-Datetime': '20171103T162727Z',
+        'Content-type': 'application/json'
+    }
+    url = "/api/v1/jobs"
+    query_string = "limit=100&offset=1"
+    payload = {}
+    expected_signature = "811f7ceb089872cd264fc5859cffcd6ddfbe8ce851f0743199ad4c96470c6b6b"
+    assert signature.calculate_signature(
+        secret,
+        method,
+        headers,
+        url,
+        query_string,
+        payload) == expected_signature
+
+
+def test_get_signature_from_headers():
+    headers = {
+        'Authorization': 'DCI-HMAC-SHA256 811f7ceb089872cd264fc5859cffcd6ddfbe8ce851f0743199ad4c96470c6b6b'
+    }
+    expected_signature = "811f7ceb089872cd264fc5859cffcd6ddfbe8ce851f0743199ad4c96470c6b6b"
+    assert signature.get_signature_from_headers(headers) == expected_signature
+
+
+def test_get_signature_from_headers_return_empty_string_if_no_auth():
+    assert signature.get_signature_from_headers({}) == ''
+
+
+def test_get_signature_from_headers_return_empty_string_if_auth_not_dci_hmac():
+    headers = {
+        'Authorization': 'Basic QWxhZGRpbjpPcGVuU2VzYW1l'
+    }
+    assert signature.get_signature_from_headers(headers) == ''
+
+
+def test_get_ordered_query_string():
+    params = {'limit': 100, 'offset': 1}
+    expected_query_string = "limit=100&offset=1"
+    query_string = signature._get_sorted_query_string(params)
+    assert expected_query_string == query_string
+
+
+@freeze_time("20171103T162727Z")
+def test_generate_headers_with_secret():
+    secret = "Y4efRHLzw2bC2deAZNZvxeeVvI46Cx8XaLYm47Dc019S6bHKejSBVJiGAfHbZLIN"
+    method = "GET"
+    content_type = 'application/json'
+    url = "/api/v1/jobs"
+    params = {'limit': 100, 'offset': 1}
+    payload = {}
+    expected_headers = {
+        'Authorization': 'DCI-HMAC-SHA256 811f7ceb089872cd264fc5859cffcd6ddfbe8ce851f0743199ad4c96470c6b6b',
+        'Content-Type': 'application/json',
+        'DCI-Datetime': '20171103T162727Z'
+    }
+    headers = signature.generate_headers_with_secret(
+        secret,
+        method,
+        content_type,
+        url,
+        params,
+        payload)
+    for key in expected_headers.keys():
+        assert expected_headers[key] == headers[key]
